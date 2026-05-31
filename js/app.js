db3c4bef5ade12bae7b884956503c383e6e5f068
sylma <sylmarvyctor@gmail.com>
2026-05-31 12:10:42 -0300

    feat: estrutura inicial do app São João de Arcoverde

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const slides =
+document.querySelectorAll(".slide");
+
+let atual = 0;
+
+function trocarSlide(){
+
+slides[atual].classList.remove("active");
+
+atual++;
+
+if(atual >= slides.length){
+
+atual = 0;
+
+}
+
+slides[atual].classList.add("active");
+
+}
+
+setInterval(trocarSlide,4000);
---
+++ js/quiz.js
@@ -0,0 +1,19 @@
+const perguntas = [
+
+{
+pergunta:
+"Em que estado fica Arcoverde?",
+
+resposta:
+"Pernambuco"
+},
+
+{
+pergunta:
+"Qual a principal festa?",
+
+resposta:
+"São João"
+}
+
+];
